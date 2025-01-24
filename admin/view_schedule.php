ed9b5c57c44d7f3b250c495750aa209cf304aeb7
= <=>
2025-01-25 02:59:23 +0300

    Update view_schedule.php to include room details in schedule display

--- admin/view_schedule.php
+++ admin/view_schedule.php
@@ -1,17 +1,18 @@
 <?php include 'db_connect.php' ?>
 <?php
 if(isset($_GET['id'])){
-	$qry = $conn->query("SELECT * FROM schedules where id=".$_GET['id'])->fetch_array();
+	$qry = $conn->query("SELECT s.*, r.room_name, r.room_type FROM schedules s 
+		INNER JOIN rooms r ON s.room_id = r.id 
+		WHERE s.id=".$_GET['id'])->fetch_array();
 	foreach($qry as $k =>$v){
 		$$k = $v;
 	}
 }
-
 ?>
 <div class="container-fluid">
 	<p>Schedule for: <b><?php echo ucwords($title) ?></b></p>
 	<p>Description: <b><?php echo $description ?></b></p>
-	<p>Location: </i> <b><?php echo $location ?></b></p>
+	<p>Room: </i> <b><?php echo "[".ucwords($room_type)."] ".$room_name ?></b></p>
 	<p>Time Start: </i> <b><?php echo date('h:i A',strtotime("2020-01-01 ".$time_from)) ?></b></p>
 	<p>Time End: </i> <b><?php echo date('h:i A',strtotime("2020-01-01 ".$time_to)) ?></b></p>
 	<hr class="divider">
